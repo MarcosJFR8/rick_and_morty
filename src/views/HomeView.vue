38715129d027a7f536db104555a239d0e2a55b5d
Marcos <marcosjfr3@gmail.com>
2025-02-08 17:04:11 -0400

    first commit

---
+++ src/views/HomeView.vue
@@ -0,0 +1,19 @@
+<script setup lang="ts">
+import { onMounted } from 'vue'
+
+const loadCharacters = async () => {
+  const response = await fetch('https://rickandmortyapi.com/api/character')
+  const data = await response.json()
+  console.log(data)
+}
+
+onMounted(() => {
+  loadCharacters()
+})
+</script>
+
+<template>
+  <main>
+    hola
+  </main>
+</template>
