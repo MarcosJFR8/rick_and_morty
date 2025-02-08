c48dc7874e8816a959a3348bf01651c9b8c4cb78
Marcos <marcosjfr3@gmail.com>
2025-02-08 18:59:18 -0400

    characters added

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,10 +1,12 @@
 <script setup lang="ts">
-import { onMounted } from 'vue'
+import { onMounted, ref } from 'vue'
+
+const characters = ref([])
 
 const loadCharacters = async () => {
   const response = await fetch('https://rickandmortyapi.com/api/character')
   const data = await response.json()
-  console.log(data)
+  characters.value = data.results
 }
 
 onMounted(() => {
@@ -14,6 +16,20 @@ onMounted(() => {
 
 <template>
   <main>
-    hola
+
+    <div class="flex justify-center items-center mt-8">
+      <h1 class="text-3xl font-bold">
+        Rick y Morty!!
+      </h1>
+    </div>
+
+    <div class="grid grid-cols-6 gap-7 mt-24 mx-10">
+      <div v-for="character in characters" :key="character.id" class="rounded-2xl overflow-hidden shadow-2xl">
+        <img class="rounded-t-3xl" :src="character.image" alt="character.name">
+        <div class="m-4 text-center">
+          {{ character.name }}
+        </div>
+      </div>
+    </div>
   </main>
 </template>
